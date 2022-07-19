5d8f170cdb7a32ef2e683638ee7a1d64a65b1de2
Awa Awa <awa.innocent.ndah@gmail.com>
2022-07-19 15:58:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "compute" {
   public_subnet  = module.networking.public_subnet
   elb            = module.loadbalancing.elb
   alb_tg         = module.loadbalancing.alb_tg
-  key_name       = "project"
+  key_name       = "alpha1omega"
 }
 
 module "loadbalancing" {
